56993927ce761883647783a66343caa11d16f8f0
AaronWisneski <awisneski@ltu.edu>
2023-11-28 15:06:59 -0500

    conveyorblocks

--- Assets/Scripts/ConveyorBlocks.cs
+++ Assets/Scripts/ConveyorBlocks.cs
@@ -6,7 +6,7 @@ public class ConveyorBlocks : MonoBehaviour
 {
     //the player's rigidbody
     public Rigidbody2D PlayerRB;
-
+    public float strength = 50.0f;
 
     [Header("True is Left, False is Right")]
     public bool Direction = true;
@@ -18,15 +18,18 @@ public class ConveyorBlocks : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             if(Direction == true) // left
             {
-
+                PlayerRB.AddForce(Vector2.left * strength);
             }
-            else
+            else // right
+            {
+                PlayerRB.AddForce(Vector2.right * strength);
+            }
         }
     }
 }
